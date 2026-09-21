96bdfef2dcdcfee540ae12818fc8a589a7c1255f
gowthamgitlook <90568572+gowthamgitlook@users.noreply.github.com>
2026-09-21 20:33:51 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -78,3 +78,5 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   role       = aws_iam_role.eks_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+
